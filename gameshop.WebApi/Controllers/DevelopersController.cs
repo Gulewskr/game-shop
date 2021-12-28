31c6c63b76c68290b20357deddabda0f6bd461cf
Rafał <rafalgulewski@gmail.com>
2021-12-28 02:17:21 +0100

    Dodanie swagger'a

---
+++ gameshop.WebApi/Controllers/DevelopersController.cs
@@ -0,0 +1,53 @@
+﻿using gameshop.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gameshop.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DevelopersController : Controller
+    {
+        private readonly IDeveloperService _developerService;
+
+        public DevelopersController(IDeveloperService service)
+        {
+            _developerService = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            String z = "WoW Mega";
+            return Json(z);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            String z = "WoW Mega id " + id;
+            return Json(z);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(/* [FromBody] CreateDeveloper developer */)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(/* [FromBody] CreateDeveloper developer ,*/ int id)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
--- gameshop.WebApi/Startup.cs
+++ gameshop.WebApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -65,6 +66,10 @@ namespace gameshop.WebApi
                     options.UseSqlServer(Configuration.GetConnectionString("GameShopConnectionString"));
                 }
             );
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GameShop.WebAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,6 +78,8 @@ namespace gameshop.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GameShop.WebAPI v1"));
             }
 
             app.UseHttpsRedirection();
