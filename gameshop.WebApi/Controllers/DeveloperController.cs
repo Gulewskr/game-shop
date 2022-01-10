e31d9337a53c126f40b9bd1d9393747a80e66411
Rafał <rafalgulewski@gmail.com>
2022-01-10 01:26:06 +0100

    Dodanie szablonu html - ustawienie nagłówka i stopki Utworzenie kontrolera Developerów w aplikacji webowej

---
+++ gameshop.WebApi/Controllers/DeveloperController.cs
@@ -0,0 +1,73 @@
+﻿using gameshop.Infrastructure.Commands;
+using gameshop.Infrastructure.DTO;
+using gameshop.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gameshop.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeveloperController : Controller
+    {
+        private readonly IDeveloperService _developerService;
+
+        public DeveloperController(IDeveloperService service)
+        {
+            _developerService = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            IEnumerable<DeveloperDTO> z = await _developerService.GetAll();
+            return Json(z);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            DeveloperDTO z = await _developerService.Get(id);
+            return Json(z);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateCompany developer)
+        {
+            await _developerService.Add(new DeveloperDTO()
+            {
+                Name = developer.Name,
+                EMail = developer.EMail,
+                Country = developer.Country,
+                City = developer.City,
+                Address = developer.Address
+            });
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromBody] CreateCompany developer, int id)
+        {
+            await _developerService.Update(new DeveloperDTO()
+            {
+                Id = id,
+                Name = developer.Name,
+                EMail = developer.EMail,
+                Country = developer.Country,
+                City = developer.City,
+                Address = developer.Address
+            });
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _developerService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ gameshop.WebApplication/Controllers/DeveloperController.cs
@@ -0,0 +1,162 @@
+﻿using gameshop.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameshop.WebApplication.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        public IConfiguration Configuration;
+        public DeveloperController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public ContentResult GetHostUrl()
+        {
+            var r = Configuration["RestApiUrl:HostUrl"];
+            return Content(r);
+        }
+
+        private string CN()
+        {
+            return ControllerContext.RouteData.Values["controller"].ToString();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            //var token = AccountController.TokenString;
+            List<CompanyVM> list = new List<CompanyVM>();
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                System.Diagnostics.Debug.WriteLine(httpClient.DefaultRequestHeaders);
+                using (var response = await httpClient.GetAsync(_restpath))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    list = JsonConvert.DeserializeObject<List<CompanyVM>>(apiResponse);
+
+                    System.Diagnostics.Debug.WriteLine(response.StatusCode);
+                }
+            }
+
+            return View(list);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            CompanyVM ob = new CompanyVM();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{_restpath}/{id}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ob = JsonConvert.DeserializeObject<CompanyVM>(apiResponse);
+                }
+            }
+
+            return View(ob);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CompanyVM o)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            CompanyVM ob = new CompanyVM();
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(o);
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                    using (var response = await httpClient.PutAsync($"{_restpath}/{o.Id}", content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        ob = JsonConvert.DeserializeObject<CompanyVM>(apiResponse);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.DeleteAsync($"{_restpath}/{id}"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CompanyVM o)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            CompanyVM ob = new CompanyVM();
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(o);
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                    using (var response = await httpClient.PostAsync($"{_restpath}", content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        ob = JsonConvert.DeserializeObject<CompanyVM>(apiResponse);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(_restpath);
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
